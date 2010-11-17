c40b8ce95bd2a25ce4d460ff2b55a9a46e133443
Mick Pollard <aussielunix@gmail.com>
2010-11-17 18:07:19 +1100

    add html/ dir with basic site + deploy task to cap

---
+++ html/js/countdown-config.js
@@ -0,0 +1,9 @@
+TargetDate = "01/01/2012 5:00 AM";
+BackColor = "";
+ForeColor = "#696969";
+		
+CountActive = true;
+CountStepper = -1;
+LeadingZero = true;
+DisplayFormat = "%%D%% days, %%H%% hrs. %%M%% min. %%S%% sec";
+FinishMessage = "It is finally here!";
