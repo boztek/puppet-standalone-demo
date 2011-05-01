cbabe3a8474b766de0d7adc25e8cd1e59d7778e7
Boris Gordon <boris@boztek.net>
2011-05-01 19:58:12 +1000

    Start regular PHP (5.3) module

---
+++ modules/php/manifests/init.pp
@@ -0,0 +1,39 @@
+#
+# Module: php
+#
+#  Created by  on 2011-05-01.
+#  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+#
+# Define:
+#  
+#
+# Parameters:
+#
+#   
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+# (start code)
+
+# Class: php
+#
+#
+class php {
+    # resources
+    package { ['php', 'php-cli', 'libapache2-mod-php5', 'php5-gd']:
+        ensure => installed,
+    }
+
+    # file { "/etc/php5/":
+    #     ensure => file,
+    #     
+    # }
+    
+    service { 'apache2':
+        ensure => present,
+    }
+}
+
+# (end)
