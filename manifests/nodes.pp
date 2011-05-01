26ae5518d0ac7a5695085137372135fe42c82302
Boris Gordon <boris@boztek.net>
2011-05-01 19:58:32 +1000

    start to use classes for later node includes

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -4,3 +4,17 @@ node "default" {
     include mysql
     include aegir
 }
+
+class d6 {
+}
+
+class d7 {
+    include php
+    # include mysql
+}
+
+node "d6.boztek.net" {
+}
+
+node "d7.boztek.net" {
+}
